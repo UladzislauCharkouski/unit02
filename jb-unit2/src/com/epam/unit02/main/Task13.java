e412556941d6726897921c5fe7760f55bd94ffc5
UladzislauCharkouski <61200221+UladzislauCharkouski@users.noreply.github.com>
2020-03-04 01:14:46 +0300

    Add files via upload

--- jb-unit2/src/com/epam/unit02/main/Task13.java
+++ jb-unit2/src/com/epam/unit02/main/Task13.java
@@ -3,25 +3,15 @@ package com.epam.unit02.main;
 public class Task13 {
 //Найдите наибольшую цифру данного натурального числа.
 	public static void main(String[] args) {
-		String s="3210401";
-	    //String[] a = s.split("");
-	    String x="123456789";
-	    String max = "";
-	    for(int i=0;i<s.length();i++){
-	        //x=Integer.parseInt(a[i]);
-             for(int k=0; k<x.length();k++) {
-            	 //System.out.println(s.charAt(i));
-            	 //System.out.println(x.charAt(k));
-            	 if(s.charAt(i) < x.charAt(k)){
-         			if(!max.contains(String.valueOf(s.charAt(i)))){
-                         max += "\n" + s.charAt(i);
-                         }
-                 }
-            	 //System.out.println(max);
-	         }
-          //System.out.println(max);
-	     }
-	    System.out.println(max);
+		String s="12487128467";
+	    String[] a = s.split("");
+	    int max = 0;
+	    for(int i=0;i<a.length;i++){
+            if(Integer.parseInt(a[i])>max) {
+            	max=Integer.parseInt(a[i]);
+            }
+         }
+	    	System.out.println("Наибольшая цифра натурального числа =" + max);
 	}
 }
-//System.out.println(x);
+ 
